d8e0577dae69bc5ae4da820ae8ee7aeb76b609c6
Yajnadutta Mishra <yajnaduttamishra@gmail.com>
2021-08-01 19:35:46 -0500

    Final Code
    Final Code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div className="App">
+      Welcome to First website Hosting
+    </div>
+    <div>
+        <footer className="footer">
+            Created by Roni samal
+        </footer>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
